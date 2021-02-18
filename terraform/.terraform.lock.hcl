98528e206a68bb0c4337ea687ce4a603be0d05e8
Matt Krause <krausem@gmail.com>
2021-02-18 13:44:46 -0800

    syntax change

---
+++ terraform/.terraform.lock.hcl
@@ -0,0 +1,39 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/azurerm" {
+  version     = "1.27.1"
+  constraints = "1.27.1"
+  hashes = [
+    "h1:S3k5NsJtDGhdTZq6jQpSPmQ90gqL2RnC7dQKZGi0Rx4=",
+    "zh:1268724f3d90cdb817c352f468546f79eb2fabd6ea7d340224d56d8de44f0ea6",
+    "zh:1df08ce5cfb50485dc3f7e27b0e838f925bc80f99d7fb8192caf6a4b4cd1a598",
+    "zh:39bdd6bce87e5c11e5da9fbacdfb13315a2bb9c97eef54e0689859c89d1a44be",
+    "zh:745418be600488e763458d1ace072edbb6c75c8e442fed593f44217d7e4c610a",
+    "zh:849ac36f7e49bd9b11dd2c8913f13b307a8eebac5d9d6ba33a5a7a7f7b37a281",
+    "zh:865499cb38dbc256b462d6b3f54931a9ea769cb32009c645eefdc0da127de643",
+    "zh:8c0a395c5e8b403810e96e6411efe002d2bd5071e6fee597b9afcc03f54f3002",
+    "zh:afbbbe0f9870aa2cb7a9bfbb4c4103e1d9fd15eed82d83e118192b16fad92dbb",
+    "zh:ce8fe9cfb8c2531fc6f3a88c2b8220db7097764a0446b472ff0224a640fe790a",
+    "zh:cfc07c49c1eed8c875768ab4dc86f620cd1f0e327d7746c86b483376be152794",
+    "zh:e1dbc839f4f1925d7031bfc142ac3fbe33e6948500e908797b4932bc87744648",
+    "zh:e7728bf68b7d44714b63be25a962896871d5c99fd814acd4ea706c54ce37f3ff",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/http" {
+  version = "2.0.0"
+  hashes = [
+    "h1:BDwvm4qfII2x/pH/VU0rFRpuS3sj5XLyX2ba/J8slQg=",
+    "zh:066b5f4f80013dcc7d94d7d4b5537607c74f93e8a414ac993709d138a36d7db6",
+    "zh:41c1916d44c263828d39eeb12ab0bea8507e4e41af6f3376cb3f2b93158e35a6",
+    "zh:78a429752ae9c3587a4ccf106ea17a89918732511f45f99ec905d014f88a3e88",
+    "zh:8be68e5e4f095a090534594d1e0e08a8ff76638fc94cb38e5e38a683b8cb62ec",
+    "zh:b00a3d86b86ac07347cd3d89f8bbb966686d897a7c37a18a69b715e8e01a4728",
+    "zh:b9aab522396439716fb2e2cacf97c4083835fd0f8ea15b95d0a754578253c66c",
+    "zh:c475bfacb7da999ff5a652680e7a7969b63c1e994462936a1ac7b98708e4ea1f",
+    "zh:d85f228d5fb4d4197972939d27a00cc6f48309a3d562de44eade347c70f02f83",
+    "zh:d91ece204cd2aec195dc7e0d1a620ceb19dcc2a64072af134fc9e49f233abb3e",
+    "zh:faa5c9801b6686872908fb55d5feb4952168dd53e0dadec0962768e24b93116a",
+  ]
+}
--- terraform/main.tf
+++ terraform/main.tf
@@ -20,66 +20,66 @@ data "http" "my_public_ip" {
 
 # Configure the provider
 provider "azurerm" {
-  version                     = "1.27.1"
-  subscription_id             = "${var.subscription_id}"
-  client_id                   = "${var.client_id}"
-  client_certificate_path     = "${var.client_certificate_path}"
-  client_certificate_password = "${var.client_certificate_password}"
-  tenant_id                   = "${var.tenant_id}"
+  # version                     = "1.27.1"
+  subscription_id             = var.subscription_id
+  client_id                   = var.client_id
+  client_certificate_path     = var.client_certificate_path
+  client_certificate_password = var.client_certificate_password
+  tenant_id                   = var.tenant_id
 }
 
 # Create a new resource group
 resource "azurerm_resource_group" "eve_rg" {
     name     = "eve-ng"
-    location = "${var.region}"
+    location = var.region
 }
 
 # Create a virtual network
 resource "azurerm_virtual_network" "eve_vnet" {
     name                = "eve-vnet"
     address_space       = ["10.0.0.0/23"]
-    location            = "${var.region}"
-    resource_group_name = "${azurerm_resource_group.eve_rg.name}"
+    location            = var.region
+    resource_group_name = azurerm_resource_group.eve_rg.name
 }
 
 # Create subnet
 resource "azurerm_subnet" "eve_subnet" {
     name                 = "eve-subnet"
-    resource_group_name  = "${azurerm_resource_group.eve_rg.name}"
-    virtual_network_name = "${azurerm_virtual_network.eve_vnet.name}"
+    resource_group_name  = azurerm_resource_group.eve_rg.name
+    virtual_network_name = azurerm_virtual_network.eve_vnet.name
     address_prefix       = "10.0.1.0/24"
 }
 
 # Create public IP
 resource "azurerm_public_ip" "eve_public_ip" {
     name                         = "eve-public-ip"
-    location                     = "${var.region}"
-    resource_group_name          = "${azurerm_resource_group.eve_rg.name}"
+    location                     = var.region
+    resource_group_name          = azurerm_resource_group.eve_rg.name
     allocation_method = "Static"
-    domain_name_label = "${var.name}"
+    domain_name_label = var.name
 }
 
 # Create network interface
 resource "azurerm_network_interface" "eve_nic" {
     name                      = "eve-nic"
-    location                  = "${var.region}"
-    resource_group_name       = "${azurerm_resource_group.eve_rg.name}"
-    network_security_group_id = "${azurerm_network_security_group.eve_nsg.id}"
+    location                  = var.region
+    resource_group_name       = azurerm_resource_group.eve_rg.name
+    network_security_group_id = azurerm_network_security_group.eve_nsg.id
 
     ip_configuration {
         name                          = "eve-nic-config"
-        subnet_id                     = "${azurerm_subnet.eve_subnet.id}"
+        subnet_id                     = azurerm_subnet.eve_subnet.id
         private_ip_address_allocation = "Static"
-        private_ip_address            = "${var.vm_ip}"
-        public_ip_address_id          = "${azurerm_public_ip.eve_public_ip.id}"
+        private_ip_address            = var.vm_ip
+        public_ip_address_id          = azurerm_public_ip.eve_public_ip.id
     }
 }
 
 # Create Network Security Group and rule
 resource "azurerm_network_security_group" "eve_nsg" {
     name                = "eve-network-security-group"
-    location            = "${var.region}"
-    resource_group_name = "${azurerm_resource_group.eve_rg.name}"
+    location            = var.region
+    resource_group_name = azurerm_resource_group.eve_rg.name
 
     security_rule {
         name                       = "SSH"
@@ -89,8 +89,8 @@ resource "azurerm_network_security_group" "eve_nsg" {
         protocol                   = "Tcp"
         source_port_range          = "*"
         destination_port_range     = "22"
-        source_address_prefix      = "${data.http.my_public_ip.body}"
-        destination_address_prefix = "${var.vm_ip}"
+        source_address_prefix      = data.http.my_public_ip.body
+        destination_address_prefix = var.vm_ip
     }
     security_rule {
         name                       = "HTTPS"
@@ -100,8 +100,8 @@ resource "azurerm_network_security_group" "eve_nsg" {
         protocol                   = "Tcp"
         source_port_range          = "*"
         destination_port_range     = "443"
-        source_address_prefix      = "${data.http.my_public_ip.body}"
-        destination_address_prefix = "${var.vm_ip}"
+        source_address_prefix      = data.http.my_public_ip.body
+        destination_address_prefix = var.vm_ip
     }
     security_rule {
         name                       = "deny"
@@ -112,26 +112,26 @@ resource "azurerm_network_security_group" "eve_nsg" {
         source_address_prefix      = "*"
         source_port_range          = "*"
         destination_port_range     = "*"
-        destination_address_prefix = "${var.vm_ip}"
+        destination_address_prefix = var.vm_ip
     }
 }
 
 resource "azurerm_managed_disk" "eve_storage" {
   name                 = "eve-storage"
-  location             = "${var.region}"
-  resource_group_name  = "${azurerm_resource_group.eve_rg.name}"
+  location             = var.region
+  resource_group_name  = azurerm_resource_group.eve_rg.name
   storage_account_type = "Standard_LRS"
   create_option        = "Empty"
-  disk_size_gb         = "${var.disk_size}"
+  disk_size_gb         = var.disk_size
 }
 
 # Create a Linux virtual machine
 resource "azurerm_virtual_machine" "eve_vm" {
     name                  = "eve-ng"
-    location              = "${var.region}"
-    resource_group_name   = "${azurerm_resource_group.eve_rg.name}"
-    network_interface_ids = ["${azurerm_network_interface.eve_nic.id}"]
-    vm_size               = "${var.vm_size}"
+    location              = var.region
+    resource_group_name   = azurerm_resource_group.eve_rg.name
+    network_interface_ids = [azurerm_network_interface.eve_nic.id]
+    vm_size               = var.vm_size
 
     storage_image_reference {
         publisher = "Canonical"
@@ -149,28 +149,28 @@ resource "azurerm_virtual_machine" "eve_vm" {
 
     os_profile {
         computer_name  = "eve-ng"
-        admin_username = "${var.vm_username}"
-        admin_password = "${var.vm_password}"
+        admin_username = var.vm_username
+        admin_password = var.vm_password
     }
 
-    os_profile_linux_config {
-        disable_password_authentication = true
-        ssh_keys {
+    #os_profile_linux_config {
+    #    disable_password_authentication = true
+    #    ssh_keys {
             #key_data = "${var.ssh_pubkey}"
-            key_data = "${file("./id_rsa.pub")}"
-            path = "/home/${var.vm_username}/.ssh/authorized_keys"
-        }
-    }
+            #key_data = file("./id_rsa.pub")
+            #path = "/home/${var.vm_username}/.ssh/authorized_keys"
+     #   }
+    #}
 
     storage_data_disk {
-        name = "${azurerm_managed_disk.eve_storage.name}"
+        name = azurerm_managed_disk.eve_storage.name
         create_option = "Attach"
-        disk_size_gb = "${var.disk_size}"
+        disk_size_gb = var.disk_size
         lun = 1
-        managed_disk_id = "${azurerm_managed_disk.eve_storage.id}"
+        managed_disk_id = azurerm_managed_disk.eve_storage.id
     }
 }
 
 output "fqdn" {
-    value = "${azurerm_public_ip.eve_public_ip.fqdn}"
+    value = azurerm_public_ip.eve_public_ip.fqdn
 }
